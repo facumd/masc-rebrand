d798fe4659d3c9fe12dd9cacfeffe7b82200b4fe
Facundo Manuel Díaz Córdoba <facudiaz1738@hotmail.com>
2023-06-09 23:34:32 -0300

    Cambios a revertir en front

---
+++ frontend/src/app/services/auth.guard.ts
@@ -0,0 +1,82 @@
+import { Injectable } from '@angular/core';
+import {
+  ActivatedRouteSnapshot,
+  CanActivate,
+  CanActivateChild,
+  CanDeactivate,
+  CanLoad,
+  ChildrenOutletContexts,
+  Route,
+  Router,
+  RouterStateSnapshot,
+  UrlSegment,
+  UrlTree,
+} from '@angular/router';
+import { Observable } from 'rxjs';
+import { AuthService } from './auth.service';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthGuard
+  implements CanActivate, CanActivateChild, CanDeactivate<unknown>, CanLoad
+{
+  constructor(private authService: AuthService, private router: Router) {}
+
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ):
+    | Observable<boolean | UrlTree>
+    | Promise<boolean | UrlTree>
+    | boolean
+    | UrlTree {
+    let url: string = state.url;
+    return this.checkLogin(route, url);
+  }
+
+  canActivateChild(
+    childRoute: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ):
+    | boolean
+    | UrlTree
+    | Observable<boolean | UrlTree>
+    | Promise<boolean | UrlTree> {
+    return this.canActivate(childRoute, state);
+  }
+
+  canDeactivate(
+    component: unknown,
+    currentRoute: ActivatedRouteSnapshot,
+    currentState: RouterStateSnapshot,
+    nextState: RouterStateSnapshot
+  ):
+    | boolean
+    | UrlTree
+    | Observable<boolean | UrlTree>
+    | Promise<boolean | UrlTree> {
+    return true;
+  }
+
+  canLoad(
+    route: Route,
+    segments: UrlSegment[]
+  ):
+    | boolean
+    | UrlTree
+    | Observable<boolean | UrlTree>
+    | Promise<boolean | UrlTree> {
+    return true;
+  }
+
+  checkLogin(route: ActivatedRouteSnapshot, url: any): boolean {
+    const userRole = this.authService.getUserPermissions();
+    if (route.data['rol'] && route.data['rol'].indexOf(userRole) === -1) {
+      this.router.navigate(['/']);
+      return false;
+    }
+
+    return true;
+  }
+}
---
+++ frontend/src/environment/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: false,
+  SITE_URL: 'http://localhost:8000',
+  API_URL: 'http://localhost:8000/api',
+};
