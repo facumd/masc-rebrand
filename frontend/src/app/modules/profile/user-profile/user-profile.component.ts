611ae939e705bdbc51a95d529ba2eb5744a26071
Facundo Manuel Díaz Córdoba <facudiaz1738@hotmail.com>
2023-06-15 19:26:37 -0300

    Borra componente de estilos sin uso

--- frontend/src/app/modules/profile/user-profile/user-profile.component.ts
+++ frontend/src/app/modules/profile/user-profile/user-profile.component.ts
@@ -6,7 +6,6 @@ import { User } from 'src/app/models/auth.model';
 @Component({
   selector: 'app-user-profile',
   templateUrl: './user-profile.component.html',
-  styleUrls: ['./user-profile.component.css'],
 })
 export class UserProfileComponent {
   user!: User;
